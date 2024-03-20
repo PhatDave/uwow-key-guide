8e5bf28e8bd3179d06807c03045ce86b915a3319
David Majdandžić <david.majdandzic@hotmail.com>
2024-03-20 17:41:01 +0100

    Update packages

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -2,18 +2,16 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      postcss: true
-    })
-  ],
+export default {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [preprocess({
+		postcss: true
+	})],
 
-  kit: {
-    adapter: adapter()
-  }
+	extensions: ['.svelte'],
+
+	kit: {
+		adapter: adapter()
+	}
 };
-
-export default config;
